6d2f77c22622fd81b5290099ae81c6690931de36
amylokh <amylokh@gmail.com>
2020-10-04 22:29:51 +0530

    Feat: User cannot register with same email

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,24 +4,38 @@ const jwt = require('jsonwebtoken');
 
 const register = (req, res, next) => {
 
-    bcrypt.hash(req.body.password, 10, (err, hashedPassword)=> {
-        if (err){
-            res.json({error: err});
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) {
+            res.json({ error: err });
         }
 
-        let user = new User({
+        let newUser = new User({
             name: req.body.name,
-            email : req.body.email,
+            email: req.body.email,
             phone: req.body.phone,
-            password : hashedPassword
+            password: hashedPassword
         });
-    
-        user.save()
-            .then(user=>{
-                res.json({message: 'User registered successfully!'});
+
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (user) {
+                    res.status(400);
+                    res.json({ message: 'Email already exists' });
+                }
+                else {
+                    newUser.save()
+                        .then(user => {
+                            res.json({ message: 'User registered successfully!' });
+                        })
+                        .catch(err => {
+                            res.status(500);
+                            res.json({ message: 'An error occurred while registering new user' });
+                        })
+                }
             })
-            .catch(err=> {
-                res.json({message: 'An error occurred while registering new user'});
+            .catch(err => {
+                res.status(500);
+                res.json({ message: 'An error occurred while finding existing username' });
             })
     });
 };
@@ -30,31 +44,32 @@ const login = (req, res, next) => {
     var username = req.body.username;
     var password = req.body.password;
 
-    User.findOne({$or: [{email: username}, {phone: username}]})
-        .then(user=> {
-           if (user) {
-               bcrypt.compare(password, user.password, function (err, result) {
-                   if (err) {
-                       console.log('error: ' + err);
-                       res.json({error: err});
-                   }
-                   if (result) {
-                       let idtoken = jwt.sign({email: user.email}, 'verySecretValue', {expiresIn: '1h'});
-                       res.json({
-                           message: 'User login successful',
-                           idtoken
-                       });
-                   }
-                   else {
-                       res.json({message: 'Incorrect password'});
-                   }
-               })
-           }
-           else {
-               res.json({message: 'No user found'});
-           }
+    User.findOne({ $or: [{ email: username }, { phone: username }] })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(password, user.password, function (err, result) {
+                    if (err) {
+                        console.log('error: ' + err);
+                        res.json({ error: err });
+                    }
+                    if (result) {
+                        let idtoken = jwt.sign({ email: user.email }, 'verySecretValue', { expiresIn: '1h' });
+                        res.json({
+                            message: 'User login successful',
+                            idtoken
+                        });
+                    }
+                    else {
+                        res.status(400);
+                        res.json({ message: 'Incorrect password' });
+                    }
+                })
+            }
+            else {
+                res.json({ message: 'No user found' });
+            }
         })
-        .catch(err=>{
+        .catch(err => {
             console.log('Some error occurred', err);
         })
 };
@@ -65,11 +80,16 @@ const verify = (req, res, next) => {
         const decode = jwt.verify(idtoken, 'verySecretValue');
 
         if (req.body.email === decode.email) {
-            res.json({message: 'Valid authentication token'});
+            res.json({ message: 'Valid authentication token' });
+        }
+        else {
+            res.status(400);
+            res.json({ message: 'Invalid authentication token provided.' });
         }
     }
-    catch(err) {
-        res.json({message: 'Invalid authentication token provided.'});
+    catch (err) {
+        res.status(400);
+        res.json({ message: 'Invalid authentication token provided.' });
     }
 }
 
