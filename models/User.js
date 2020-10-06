4ae75ae4966cf851f75204c2832ee3511bcf3574
amylokh <amylokh@gmail.com>
2020-10-06 23:23:03 +0530

    Update user model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const userSchema = new Schema({
     name : {
         type: String
     },
     email : {
-        type: String
+        type: String,
+        required: true,
+        lowercase: true,
+        match: [emailRegex, 'email format is invalid']
     },
     phone : {
-        type: String
+        type: String,
+        required: true
     },
     password : {
-        type: String
+        type: String,
+        required: true
     }
 }, {timestamps: true});
 
